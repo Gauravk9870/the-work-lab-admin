56a98cabd3f9cd8cac7649a88d880338b0aaffd0
TrippyGaurav <141822413+TrippyGaurav@users.noreply.github.com>
2024-01-21 22:38:37 +0530

    add vercel.json

--- src/pages/ogranizationDetails/OrganizationDetails.jsx
+++ src/pages/ogranizationDetails/OrganizationDetails.jsx
@@ -1,7 +1,40 @@
 import React from "react";
 import styles from "./organizationDetails.module.scss";
 
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+
 const OrganizationDetails = () => {
+  const [organizationData, setOrganizationData] = useState({});
+  const { organizationId } = useParams();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchOrganizationData = async () => {
+      try {
+        setLoading(true);
+        const response = await axios.get(
+          `${BASE_URL}/api/organization/${organizationId}`
+        );
+
+        if (response.status === 200) {
+          console.log(response.data);
+
+          setOrganizationData(response.data);
+          setLoading(false);
+        } else {
+          console.error("Failed to fetch organization data");
+          setLoading(false);
+        }
+      } catch (error) {
+        console.error("Error fetching organization data:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchOrganizationData();
+  }, [organizationId]);
+
   return (
     <div className={styles.organizationDetails}>
       <div className={styles.container}></div>
